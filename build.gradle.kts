d389099661c5ac12a9ea1f382a1aa171c6824ab3
joffrey-bion <joffrey.bion@gmail.com>
2020-10-23 17:15:05 +0200

    Remove bintray-specific metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,12 +8,6 @@ plugins {
 version = "0.1.0"
 description = "A Kotlin client for the Chrome DevTools Protocol"
 
-val Project.labels: Array<String>
-    get() = arrayOf("chrome", "devtools", "protocol", "chromedp", "kotlin", "coroutines", "async")
-
-val Project.licenses: Array<String>
-    get() = arrayOf("MIT")
-
 repositories {
     jcenter()
 }
