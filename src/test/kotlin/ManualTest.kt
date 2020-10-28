27206cc98aa748ccd1b43e4c01fa1491c535c372
joffrey-bion <joffrey.bion@gmail.com>
2020-10-28 17:43:20 +0100

    Improved manual tests to cover more use cases

--- src/test/kotlin/ManualTest.kt
+++ src/test/kotlin/ManualTest.kt
@@ -1,17 +1,21 @@
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.takeWhile
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.Serializable
-import org.hildan.chrome.devtools.domains.dom.GetDocumentRequest
 import org.hildan.chrome.devtools.domains.dom.GetOuterHTMLRequest
-import org.hildan.chrome.devtools.domains.dom.QuerySelectorRequest
+import org.hildan.chrome.devtools.domains.dom.findNodeBySelector
+import org.hildan.chrome.devtools.domains.page.events.PageEvent
 import org.hildan.chrome.devtools.domains.runtime.evaluateJs
 import org.hildan.chrome.devtools.domains.target.CloseTargetRequest
 import org.hildan.chrome.devtools.protocol.ChromeDPClient
+import org.hildan.chrome.devtools.protocol.ExperimentalChromeApi
 import org.hildan.chrome.devtools.targets.attachToNewPage
 import kotlin.test.Ignore
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 // Run chrome headless docker container first:
@@ -19,9 +23,10 @@ import kotlin.test.assertTrue
 @Ignore
 class ManualTest {
 
+    @OptIn(ExperimentalChromeApi::class)
     @Test
     fun test() {
-        runBlocking(Dispatchers.Default) {
+        runBlocking {
             val chrome = ChromeDPClient()
             chrome.closeAllTargets()
 
@@ -29,44 +34,54 @@ class ManualTest {
             assertTrue(version.browser.contains("Chrome"))
 
             val browser = chrome.webSocket()
-            val api = browser.attachToNewPage("http://www.google.com")
-            api.page.enable()
-            api.page.frameStoppedLoading().first()
-            delay(500)
-            api.dom.enable()
-            val doc = api.dom.getDocument(GetDocumentRequest()).root
-            val nodeId = api.dom.querySelector(QuerySelectorRequest(doc.nodeId, "#main")).nodeId
+            val page = browser.attachToNewPage("http://www.google.com")
+
+            page.page.enable()
+            page.dom.enable()
+            val pageJob = page.page.events().onEach { println("Page event: ${it::class.simpleName}") }.launchIn(this)
+            val domJob = page.dom.events().onEach { println("DOM event: ${it::class.simpleName}") }.launchIn(this)
+
+            page.page.frameStoppedLoading().first()
+            println("Frame stopped loading")
+
+            val nodeId = page.dom.findNodeBySelector("#main")
             println(nodeId)
-            val html = api.dom.getOuterHTML(GetOuterHTMLRequest(nodeId = nodeId))
+            val html = page.dom.getOuterHTML(GetOuterHTMLRequest(nodeId = nodeId))
             println(html)
-            browser.target.closeTarget(CloseTargetRequest(targetId = api.targetInfo.targetId))
+            browser.target.closeTarget(CloseTargetRequest(targetId = page.targetInfo.targetId))
+
+            domJob.cancel()
+            pageJob.cancel()
         }
     }
 
+    @OptIn(ExperimentalChromeApi::class)
     @Test
-    fun detachedSession_nonTargetDomain_shouldFail() {
-        runBlocking(Dispatchers.Default) {
-            val chrome = ChromeDPClient()
-            chrome.closeAllTargets()
-            val target = chrome.newTab("http://www.google.com")
-            val api = target.attach()
-            println(api.browser.getVersion())
-        }
-    }
-
-    @Test
-    fun listTargets() {
+    fun page_getTargets() {
         runBlocking {
             val chrome = ChromeDPClient()
             chrome.closeAllTargets()
             val browser = chrome.webSocket()
             val page = browser.attachToNewPage("http://google.com")
-            println(page.target.getTargets())
+            println(page.targetInfo)
+            page.page.enable()
+            page.page.events()
+                .onEach { println(it) }
+                .takeWhile { it !is PageEvent.FrameStoppedLoadingEvent }
+                .launchIn(this)
+            page.page.frameStoppedLoading().first()
+
+            val targets = page.target.getTargets().targetInfos
+            assertEquals(1, targets.size) // only page targets are present
+            assertEquals("page", targets[0].type)
+            assertTrue(targets[0].attached)
+            assertTrue(targets[0].url.contains("www.google.com")) // redirected
+            println(targets)
         }
     }
 
     @Test
-    fun evaluateJs() {
+    fun runtime_evaluateJs() {
         runBlocking {
             @Serializable
             data class Person(val firstName: String, val lastName: String)
@@ -75,9 +90,15 @@ class ManualTest {
             chrome.closeAllTargets()
             val browser = chrome.webSocket()
             val page = browser.attachToNewPage("http://google.com")
-            println(page.runtime.evaluateJs<Int>("42"))
-            println(page.runtime.evaluateJs<Pair<Int, String>>("""eval({first: 42, second: "test"})"""))
-            println(page.runtime.evaluateJs<Person>("""eval({firstName: "Bob", lastName: "Lee Swagger"})"""))
+            assertEquals(42, page.runtime.evaluateJs<Int>("42"))
+            assertEquals(
+                42 to "test",
+                page.runtime.evaluateJs<Pair<Int, String>>("""eval({first: 42, second: "test"})""")
+            )
+            assertEquals(
+                Person("Bob", "Lee Swagger"),
+                page.runtime.evaluateJs<Person>("""eval({firstName: "Bob", lastName: "Lee Swagger"})""")
+            )
         }
     }
 }
