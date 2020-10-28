3390dba1dae10e8c6cecdd28b2e731e7e1e29759
joffrey-bion <joffrey.bion@gmail.com>
2020-10-28 14:26:55 +0100

    Add doc on DOM extensions

--- src/main/kotlin/org/hildan/chrome/devtools/domains/dom/DOMExtensions.kt
+++ src/main/kotlin/org/hildan/chrome/devtools/domains/dom/DOMExtensions.kt
@@ -1,16 +1,29 @@
 package org.hildan.chrome.devtools.domains.dom
 
-suspend fun DOMDomain.getRootNodeId(): NodeId {
+/**
+ * Retrieves the ID of the root node of the current document.
+ */
+suspend fun DOMDomain.getDocumentRootNodeId(): NodeId {
     val document = getDocument(GetDocumentRequest())
     return document.root.nodeId
 }
 
+/**
+ * Retrieves the ID of the node corresponding to the given [selector], or null if not found.
+ *
+ * Note that the returned [NodeId] cannot really be used to retrieve actual node information, and this is apparently
+ * [by design of the DOM domain](https://github.com/ChromeDevTools/devtools-protocol/issues/20).
+ * It can be used to perform other CDP commands that require a [NodeId], though.
+ */
 suspend fun DOMDomain.findNodeBySelector(selector: String): NodeId? {
-    val rootNodeId = getRootNodeId()
+    val rootNodeId = getDocumentRootNodeId()
     val response = querySelector(QuerySelectorRequest(rootNodeId, selector))
     return if (response.nodeId == 0) null else response.nodeId
 }
 
+/**
+ * Moves the focus to the node corresponding to the given [selector], or null if not found.
+ */
 suspend fun DOMDomain.focusNodeBySelector(selector: String) {
     val nodeId = findNodeBySelector(selector) ?: error("Cannot focus: no node found using selector '$selector'")
     focus(FocusRequest(nodeId = nodeId))
