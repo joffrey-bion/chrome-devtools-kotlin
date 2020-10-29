39c1b61595e5d3e550cbbf67d0e05c276e5e94e0
joffrey-bion <joffrey.bion@gmail.com>
2020-10-29 02:30:35 +0100

    Version 0.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "org.hildan.chrome"
-version = "0.3.0"
+version = "0.4.0"
 description = "A Kotlin client for the Chrome DevTools Protocol"
 
 val Project.labels: Array<String>
