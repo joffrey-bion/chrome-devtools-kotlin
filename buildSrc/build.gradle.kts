807f21e79cf8456e94759f2c9ce4653676f3c8b7
joffrey-bion <joffrey.bion@gmail.com>
2021-01-04 18:40:14 +0100

    Explain Kotlin version in buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,4 +1,8 @@
 plugins {
+    // Gradle doesn't support Kotlin 1.4 yet, the changes are ready but will be published in 6.8:
+    // https://github.com/gradle/gradle/issues/12660
+    // Using Kotlin 1.4 in scripts will only be available in Gradle 7 though:
+    // https://github.com/gradle/gradle/issues/14888
     kotlin("jvm") version "1.3.72"
     kotlin("plugin.serialization") version "1.3.72"
 }
