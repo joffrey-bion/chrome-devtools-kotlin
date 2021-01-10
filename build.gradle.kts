23b8ba7afd4620bd71bc8137501f2219e9b5043a
joffrey-bion <joffrey.bion@gmail.com>
2021-01-10 19:05:02 +0100

    Upgrade to Ktor 1.5
    Resolves:
    https://github.com/joffrey-bion/chrome-devtools-kotlin/issues/24

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     api("org.hildan.krossbow:krossbow-websocket-core:1.1.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
 
-    val ktorVersion = "1.4.3"
+    val ktorVersion = "1.5.0"
     api("io.ktor:ktor-client-cio:$ktorVersion")
     api("io.ktor:ktor-client-serialization-jvm:$ktorVersion")
 
