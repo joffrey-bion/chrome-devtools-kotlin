785d43925eb40ec3b8237089d3f7cbac6ab70de2
Joffrey Bion <joffrey.bion@gmail.com>
2022-08-21 00:58:42 +0200

    Remove deprecated navigateAndWaitLoading functions
    Resolves:
    https://github.com/joffrey-bion/chrome-devtools-kotlin/issues/194

--- src/main/kotlin/org/hildan/chrome/devtools/domains/page/PageExtensions.kt
+++ src/main/kotlin/org/hildan/chrome/devtools/domains/page/PageExtensions.kt
@@ -1,33 +1,7 @@
 package org.hildan.chrome.devtools.domains.page
 
-import kotlinx.coroutines.flow.first
-import org.hildan.chrome.devtools.protocol.ExperimentalChromeApi
 import java.nio.file.Path
 import java.util.*
-
-/**
- * Navigates the current page according to the provided [url], and suspends until the corresponding
- * `frameStoppedLoading` event is received.
- */
-@Deprecated("This method is subject to race conditions. Prefer calling ChromePageSession.navigateAndAwaitPageLoad")
-suspend fun PageDomain.navigateAndWaitLoading(url: String) {
-    @Suppress("DEPRECATION")
-    navigateAndWaitLoading(NavigateRequest(url = url))
-}
-
-/**
- * Navigates the current page according to the provided [navigateRequest], and suspends until the
- * corresponding `frameStoppedLoading` event is received.
- */
-@Deprecated("This method is subject to race conditions. Prefer calling ChromePageSession.navigateAndAwaitPageLoad")
-@OptIn(ExperimentalChromeApi::class)
-suspend fun PageDomain.navigateAndWaitLoading(navigateRequest: NavigateRequest) {
-    enable()
-    val frameStoppedLoadingEvents = frameStoppedLoadingEvents()
-    val frameId = navigate(navigateRequest).frameId
-    // FIXME if we're too slow here, we can miss the frame event
-    frameStoppedLoadingEvents.first { it.frameId == frameId }
-}
 
 /**
  * Captures a screenshot of the current page based on the given [options], and stores the resulting image into a new
