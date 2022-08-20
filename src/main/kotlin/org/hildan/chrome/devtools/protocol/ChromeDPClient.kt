32825d9bf272b098944e6ce9e16ea03708408f13
Joffrey Bion <joffrey.bion@gmail.com>
2022-08-20 22:33:46 +0200

    Remove deprecated ChromeDPTarget.attach() method
    Resolves:
    https://github.com/joffrey-bion/chrome-devtools-kotlin/issues/192

--- src/main/kotlin/org/hildan/chrome/devtools/protocol/ChromeDPClient.kt
+++ src/main/kotlin/org/hildan/chrome/devtools/protocol/ChromeDPClient.kt
@@ -177,16 +177,7 @@ data class ChromeDPTarget(
     val description: String,
     val devtoolsFrontendUrl: String,
     val webSocketDebuggerUrl: String,
-) {
-    /**
-     * Attaches to this target via a new web socket connection to this target's debugger URL.
-     * This establishes a new protocol session to this target.
-     */
-    @Deprecated("this method provides no way of closing the created websocket connection, prefer using " +
-        "ChromeDPClient.webSocket and using the richer websocket API to interact with targets (ChromeBrowserSession.target)")
-    suspend fun attach(webSocketClient: WebSocketClient = DEFAULT_WEBSOCKET_CLIENT): ChromePageSession =
-        webSocketClient.connectToChrome(webSocketDebuggerUrl).attachToPage(id)
-}
+)
 
 /**
  * Connects to the Chrome debugger at the given [webSocketDebuggerUrl].
