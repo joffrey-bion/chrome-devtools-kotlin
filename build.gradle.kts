907f59f7a8c2df3cd1838a9866143627706d6991
Joffrey Bion <joffrey.bion@gmail.com>
2022-11-01 02:08:08 +0100

    Upgrade Github changelog generator to 1.12.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     `maven-publish`
     signing
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
-    id("org.hildan.github.changelog") version "1.11.1"
+    id("org.hildan.github.changelog") version "1.12.1"
     id("ru.vyarus.github-info") version "1.4.0"
 }
 
