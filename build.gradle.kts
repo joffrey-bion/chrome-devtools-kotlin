0b873e89e437dd932c797f1ad994e8478b0ce1c8
Joffrey Bion <joffrey.bion@gmail.com>
2022-11-13 01:53:03 +0100

    Upgrade kotlinx serialization 1.4.1
    Resolves:
    https://github.com/joffrey-bion/chrome-devtools-kotlin/issues/209

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 
     val ktorVersion = "2.1.2"
     api("io.ktor:ktor-client-cio:$ktorVersion")
