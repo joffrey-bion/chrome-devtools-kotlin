ab2b6ad3828f44b6f080064e0340c607ff913956
Joffrey Bion <joffrey.bion@gmail.com>
2023-03-12 00:01:02 +0100

    Add build scan publishing to CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,40 @@
+import com.gradle.scan.plugin.BuildScanExtension
+
+plugins {
+    id("com.gradle.enterprise") version "3.12.4"
+}
+
 rootProject.name = "chrome-devtools-kotlin"
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+
+        val isCIBuild = !System.getenv("CI").isNullOrEmpty()
+        publishAlwaysIf(isCIBuild)
+        tag(if (isCIBuild) "CI" else "local")
+
+        val isGithubActionsBuild = !System.getenv("GITHUB_ACTIONS").isNullOrEmpty()
+        if (isGithubActionsBuild) {
+            addGithubActionsData()
+        }
+    }
+}
+
+fun BuildScanExtension.addGithubActionsData() {
+    value("GitHub Event", System.getenv("GITHUB_EVENT_NAME"))
+    value("GitHub Workflow", System.getenv("GITHUB_WORKFLOW"))
+    value("GitHub Run ID", System.getenv("GITHUB_RUN_ID"))
+    value("GitHub Run number", System.getenv("GITHUB_RUN_NUMBER"))
+    value("Commit", System.getenv("GITHUB_SHA"))
+
+    val ref = System.getenv("GITHUB_REF") ?: ""
+    val isTagBuild = ref.startsWith("refs/tags/")
+    if (isTagBuild) {
+        tag("tag")
+        value("Tag", ref.removePrefix("refs/tags/"))
+    } else {
+        value("Branch", ref.removePrefix("refs/heads/"))
+    }
+}
