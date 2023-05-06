ace83da2725950431863168c4be2e1b8256e0ef2
Joffrey Bion <joffrey.bion@gmail.com>
2023-05-07 01:22:53 +0200

    Fix awaitNodeBySelector so it doesn't trigger IAE on cancellation
    Resolves:
    https://github.com/joffrey-bion/chrome-devtools-kotlin/issues/239
    Resolves:
    https://github.com/joffrey-bion/chrome-devtools-kotlin/issues/243

--- src/commonMain/kotlin/org/hildan/chrome/devtools/domains/dom/DOMExtensions.kt
+++ src/commonMain/kotlin/org/hildan/chrome/devtools/domains/dom/DOMExtensions.kt
@@ -1,9 +1,8 @@
 package org.hildan.chrome.devtools.domains.dom
 
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
-import kotlin.coroutines.coroutineContext
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 
 /**
  * A CSS selector string, as [defined by the W3C](https://www.w3schools.com/cssref/css_selectors.asp).
@@ -30,6 +29,11 @@ suspend fun DOMDomain.getDocumentRootNodeId(): NodeId = getDocumentRoot().nodeId
 suspend fun DOMDomain.findNodeBySelector(selector: CssSelector): NodeId? =
     querySelectorOnNode(getDocumentRootNodeId(), selector)
 
+private suspend fun DOMDomain.querySelectorOnNode(nodeId: NodeId, selector: CssSelector): NodeId? {
+    // for some reason, the API returns nodeId == 0 when not found
+    return querySelector(nodeId, selector).nodeId.takeIf { it != 0 }
+}
+
 /**
  * Retrieves the ID of the node corresponding to the given [selector], and retries until there is a match using the
  * given [pollingPeriod].
@@ -42,37 +46,26 @@ suspend fun DOMDomain.findNodeBySelector(selector: CssSelector): NodeId? =
  * [by design of the DOM domain](https://github.com/ChromeDevTools/devtools-protocol/issues/20).
  * It can be used to perform other CDP commands that require a [NodeId], though.
  */
-suspend fun DOMDomain.awaitNodeBySelector(selector: CssSelector, pollingPeriod: Duration): NodeId =
-    awaitNodeBySelector(selector, pollingPeriod.inWholeMilliseconds)
-
-/**
- * Retrieves the ID of the node corresponding to the given [selector], and retries until there is a match using the
- * given [pollingPeriodMillis].
- *
- * This method may suspend forever if the [selector] never matches any node.
- * The caller is responsible for using [withTimeout][kotlinx.coroutines.withTimeout] or similar cancellation mechanisms
- * around calls to this method if handling this case is necessary.
- *
- * Note that the returned [NodeId] cannot really be used to retrieve actual node information, and this is apparently
- * [by design of the DOM domain](https://github.com/ChromeDevTools/devtools-protocol/issues/20).
- * It can be used to perform other CDP commands that require a [NodeId], though.
- */
-suspend fun DOMDomain.awaitNodeBySelector(selector: CssSelector, pollingPeriodMillis: Long = 200): NodeId {
-    while (coroutineContext.isActive) {
+suspend fun DOMDomain.awaitNodeBySelector(selector: CssSelector, pollingPeriod: Duration = 200.milliseconds): NodeId {
+    while (true) {
         // it looks like we do need to get a new document at each poll otherwise we may not see the new nodes
         val nodeId = findNodeBySelector(selector)
         if (nodeId != null) {
             return nodeId
         }
-        delay(pollingPeriodMillis)
+        delay(pollingPeriod)
     }
-    error("Cancelled while awaiting node by selector \"$selector\"")
 }
 
-private suspend fun DOMDomain.querySelectorOnNode(nodeId: NodeId, selector: CssSelector): NodeId? {
-    val response = querySelector(nodeId, selector)
-    return if (response.nodeId == 0) null else response.nodeId
-}
+@Deprecated(
+    message = "Prefer the overload using kotlin.time.Duration",
+    replaceWith = ReplaceWith(
+        expression = "this.awaitNodeBySelector(selector, pollingPeriodMillis.milliseconds)",
+        imports = ["kotlin.time.Duration.Companion.milliseconds"],
+    ),
+)
+suspend fun DOMDomain.awaitNodeBySelector(selector: CssSelector, pollingPeriodMillis: Long): NodeId =
+    awaitNodeBySelector(selector, pollingPeriodMillis.milliseconds)
 
 /**
  * Retrieves the ID of the node corresponding to the given [selector], or throw an exception if not found.
