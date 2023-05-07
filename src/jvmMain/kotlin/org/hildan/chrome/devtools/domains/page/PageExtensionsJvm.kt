c294e981b4ed0e0a6918ad3125c2cdfd2cb62288
Joffrey Bion <joffrey.bion@gmail.com>
2023-05-07 17:30:35 +0200

    Remove unnecessary opt-in

--- src/jvmMain/kotlin/org/hildan/chrome/devtools/domains/page/PageExtensionsJvm.kt
+++ src/jvmMain/kotlin/org/hildan/chrome/devtools/domains/page/PageExtensionsJvm.kt
@@ -1,14 +1,12 @@
 package org.hildan.chrome.devtools.domains.page
 
 import java.nio.file.Path
-import kotlin.io.encoding.ExperimentalEncodingApi
 import kotlin.io.path.writeBytes
 
 /**
  * Captures a screenshot of the current page based on the given [options], and stores the resulting image into a new
  * file at the given [outputImagePath]. If the file already exists, it is overwritten.
  */
-@OptIn(ExperimentalEncodingApi::class)
 suspend fun PageDomain.captureScreenshotToFile(
     outputImagePath: Path,
     options: CaptureScreenshotRequest.Builder.() -> Unit = {},
