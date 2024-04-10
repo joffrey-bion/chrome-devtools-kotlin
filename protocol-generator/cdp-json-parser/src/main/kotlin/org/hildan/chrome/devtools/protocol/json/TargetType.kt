1a07fda849a2c1b2f38d2869d505292be53a512b
Joffrey Bion <joffrey.bion@gmail.com>
2024-04-10 03:29:25 +0200

    Update target types by reading Chromium's code

--- protocol-generator/cdp-json-parser/src/main/kotlin/org/hildan/chrome/devtools/protocol/json/TargetType.kt
+++ protocol-generator/cdp-json-parser/src/main/kotlin/org/hildan/chrome/devtools/protocol/json/TargetType.kt
@@ -4,50 +4,55 @@ import kotlinx.serialization.*
 import kotlinx.serialization.json.*
 import java.nio.file.Path
 
-// Data for this is read from protocol/target_types.json, which is manually extracted from Chromium's source.
-// The doc on individual properties explains how they can be found in the source.
-
 /**
  * A target type in the general sense of "a Chromium devtools_agent_host implementation".
  * 
  * This is different from the protocol's notion of "target type". The `TargetInfo.type` field
  * [in the protocol definition](https://chromedevtools.github.io/devtools-protocol/tot/Target/#type-TargetInfo) is a
  * more fine-grained segregation of targets. Each Chromium target type implementation can actually support multiple 
- * protocol target types (see [supportedCdpTargets]). The possible protocol target types are actually defined in terms
- * of Chromium's implementation instead of being formally specified (I have stopped losing my mind over it).
+ * protocol target types (see [supportedCdpTargets]).
  *
  * In addition to that, each Chromium target type implementation supports a different set of protocol domains, and this
- * is NOT part of the protocol definition (alas!).
+ * information is not available in the protocol definition (alas!).
  * 
  * In Chromium's code, multiple `<name>_devtools_agent_host.cc` files correspond to these Chromium target types, and 
  * they register different sets of domains, and can handle different protocol target types. We have to read the sources
- * to know which domains and which target types are supported.
+ * to know which domains and which target types are supported (see protocol/README.md).
  */
 @Serializable
 data class TargetType(
     /**
-     * The Chromium source file defining this target type.
-     */
-    val chromiumSourceFile: String,
-    /**
-     * The name of this target type in the Kotlin representation of chrome-devtools-kotlin.
+     * The name chosen for this target type in the Kotlin representation of chrome-devtools-kotlin.
      */
     val kotlinName: String,
     /**
-     * Target type names (as returned by the protocol) supported by this Chromium target type.
+     * The class in Chromium sources defining this target type.
+     */
+    val chromiumAgentHostType: String,
+    /**
+     * Target type names (as returned by the protocol in `TargetInfo.type`) supported by this Chromium target type.
      *
      * This is inferred from usages of the `const char DevToolsAgentHost`
-     * [constants in Chromium's source](https://source.chromium.org/chromium/chromium/src/+/main:content/browser/devtools/devtools_agent_host_impl.cc;l=126-140)
+     * [constants in Chromium's source](https://source.chromium.org/chromium/chromium/src/+/main:content/browser/devtools/devtools_agent_host_impl.cc?q=%22const%20char%20DevToolsAgentHost::kType%22)
      */
     val supportedCdpTargets: List<String>,
     /**
-     * The domains supported by this target type.
-     *
-     * This is inferred from the
-     * [handler declarations in Chromium's source](https://source.chromium.org/search?q=%22session-%3ECreateAndAddHandler%22%20f:devtools&ss=chromium).
+     * The domains supported by this target type, as found in Chromium's sources.
+     * 
+     * This is based on the [registration of domain handlers in Chromium's source](https://source.chromium.org/search?q=%22session-%3ECreateAndAddHandler%22%20f:devtools&ss=chromium).
      */
-    val supportedDomains: List<String>,
+    val supportedDomainsInChromium: List<String>,
+    /**
+     * Additional domains that are effectively supported by this target type, despite not being found in Chromium
+     * sources. They are demonstrably present based on integration tests with the zenika/alpine-chrome Docker image.
+     */
+    val additionalSupportedDomains: List<String> = emptyList(),
 ) {
+    /**
+     * The domains supported by this target type.
+     */
+    val supportedDomains = supportedDomainsInChromium + additionalSupportedDomains
+    
     companion object {
         fun parseJson(path: Path): List<TargetType> = Json.decodeFromString(path.toFile().readText())
     }
