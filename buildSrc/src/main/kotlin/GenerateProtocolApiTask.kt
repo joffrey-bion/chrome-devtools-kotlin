c37c37f0b68ee40a1ba4320576f4923f1d31e9ca
Joffrey Bion <joffrey.bion@gmail.com>
2024-06-09 14:39:45 +0200

    Add description and cleanup a bit the GenerateProtocolApiTask

--- buildSrc/src/main/kotlin/GenerateProtocolApiTask.kt
+++ buildSrc/src/main/kotlin/GenerateProtocolApiTask.kt
@@ -1,34 +1,41 @@
 package org.hildan.chrome.devtools.build
 
 import org.gradle.api.*
+import org.gradle.api.file.*
 import org.gradle.api.tasks.*
 import org.hildan.chrome.devtools.protocol.generator.*
+import java.io.*
 
 @CacheableTask
 abstract class GenerateProtocolApiTask : DefaultTask() {
 
     init {
         group = "protocol"
+        description = "Generates protocol API classes (domains, requests, responses, target types...)"
     }
 
     @PathSensitive(PathSensitivity.RELATIVE)
     @InputFiles
-    val protocolPaths = project.files(
+    val protocolPaths: ConfigurableFileCollection = project.files(
         "protocol-definition/browser_protocol.json",
         "protocol-definition/js_protocol.json",
     )
 
     @PathSensitive(PathSensitivity.RELATIVE)
     @InputFile
-    val targetTypesPath = project.file("protocol-definition/target_types.json")
+    val targetTypesPath: File = project.file("protocol-definition/target_types.json")
 
     @OutputDirectory
-    val outputDirPath = project.file("src/commonMain/generated")
+    val outputDirPath: File = project.file("src/commonMain/generated")
 
     @TaskAction
     fun generate() {
         println("Generating Chrome DevTools Protocol API...")
-        Generator(protocolPaths.map { it.toPath() }, targetTypesPath.toPath(), outputDirPath.toPath()).generate()
+        Generator(
+            protocolFiles = protocolPaths.map { it.toPath() },
+            targetTypesFile = targetTypesPath.toPath(),
+            generatedSourcesDir = outputDirPath.toPath(),
+        ).generate()
         println("Done.")
     }
 }
