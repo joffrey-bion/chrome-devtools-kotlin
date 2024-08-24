07cd0e755ea278fecd23702d2ae8cb6221dad3f4
Joffrey Bion <joffrey.bion@gmail.com>
2024-08-24 13:39:18 +0200

    Remove refresh versions which is now unused

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,5 @@
-import de.fayard.refreshVersions.core.StabilityLevel
-
 plugins {
     id("com.gradle.develocity") version "3.18"
-    id("de.fayard.refreshVersions") version "0.60.5"
     id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
 }
 
@@ -13,13 +10,6 @@ develocity {
         termsOfUseUrl = "https://gradle.com/terms-of-service"
         termsOfUseAgree = "yes"
         uploadInBackground = false // bad for CI, and not critical for local runs
-    }
-}
-
-refreshVersions {
-    versionsPropertiesFile = file("build/tmp/refreshVersions/versions.properties").apply { parentFile.mkdirs() }
-    rejectVersionIf {
-        candidate.stabilityLevel != StabilityLevel.Stable
     }
 }
 
